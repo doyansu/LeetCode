d3dc6b9a98df580408d5f8d0fa05c41afc3baa22
yuanhao <fyuanhao91@gmail.com>
2025-05-16 20:33:39 +0800

    [Algorithms-1342] c solution.

---
+++ Algorithms/C/1. Two Sum/1342. Number of Steps to Reduce a Number to Zero.c
@@ -0,0 +1,11 @@
+int numberOfSteps(int num) {
+    int step=0;
+    while(num > 0){
+        if(num&1)
+            num--;
+        else
+            num>>=1;
+        step++;
+    }
+    return step;
+}
