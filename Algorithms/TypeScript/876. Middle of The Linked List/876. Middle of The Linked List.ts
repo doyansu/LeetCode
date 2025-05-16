09a0770d8ec812fbe770c5af01bb8798629f99ed
yuanhao <fyuanhao91@gmail.com>
2025-05-16 22:44:51 +0800

    [Algorithms-876] add two new solution.

--- Algorithms/TypeScript/876. Middle of The Linked List/876. Middle of The Linked List.ts
+++ Algorithms/TypeScript/876. Middle of The Linked List/876. Middle of The Linked List.ts
@@ -25,6 +25,34 @@ function middleNode(head: ListNode | null): ListNode | null {
     return head;
 };
 
+function middleNode1(head: ListNode | null): ListNode | null {
+    let temp_node : ListNode | null = head;
+    let count : number = 1;
+    while(temp_node != null && temp_node.next !== null) {
+        count++;
+        temp_node = temp_node.next;
+        if (head != null && (count&1) == 0) {
+            head = head.next;
+        }
+    }
+    return head;
+};
+
+function middleNode2(head: ListNode | null): ListNode | null {
+    let slow : ListNode | null  = head;
+    let fast : ListNode | null = head;
+    
+    while (fast != null && fast.next != null) {
+        if (slow != null)
+            slow = slow.next;
+        fast = fast.next.next;
+    }
+    
+    return slow;
+};
+
 export const testFuntions = [ 
     middleNode, 
+    middleNode1,
+    middleNode2,
 ];
