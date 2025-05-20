2b03c8fae84b3ec0451d4151d854df0dac1a8832
yuanhao <fyuanhao91@gmail.com>
2025-05-20 17:46:32 +0800

    [Algorithms-1768] typescript solution.

---
+++ Algorithms/TypeScript/1768. Merge Strings Alternately/1768. Merge Strings Alternately.spec.ts
@@ -0,0 +1,25 @@
+﻿import { TestCase, test_template } from '../interface/testCase';
+import { test_funtions } from './1768. Merge Strings Alternately';
+
+const testCases: TestCase[] = [
+    {
+        name: 'Example 1',
+        inputs: ["abc", "pqr"],
+        expect_result: ("apbqcr"),
+    },
+    {
+        name: 'Example 2',
+        inputs: ["ab", "pqrs"],
+        expect_result: ("apbqrs"),
+    },
+    {
+        name: 'Example 3',
+        inputs: ["abcd", "pq"],
+        expect_result: ("apbqcd"),
+    },
+];
+
+test_funtions.forEach((fnc) => {
+    const template = test_template(fnc);
+    testCases.forEach((testCase) => template(testCase));
+});
---
+++ Algorithms/TypeScript/1768. Merge Strings Alternately/1768. Merge Strings Alternately.ts
@@ -0,0 +1,44 @@
+﻿// 48ms
+function mergeAlternately(word1: string, word2: string): string {
+    let merged_string = ''
+    while (word1 != '' && word2 != '') {
+        merged_string += word1[0];
+        merged_string += word2[0];
+        word1 = word1.slice(1);
+        word2 = word2.slice(1);
+    }
+    merged_string += word1 != '' ? word1 : word2;
+    return merged_string;
+};
+
+// 38ms
+function mergeAlternately1(word1: string, word2: string): string {
+    let merged_string = ''
+    while (word1 != '' && word2 != '') {
+        merged_string += word1[0];
+        merged_string += word2[0];
+        word1 = word1.substring(1);
+        word2 = word2.substring(1);
+    }
+    merged_string += word1 != '' ? word1 : word2;
+    return merged_string;
+};
+
+// 42ms
+function mergeAlternately2(word1: string, word2: string): string {
+    let merged_string = ''
+    let index = 0;
+    for (index = 0; index < word1.length && index < word2.length; ++index) {
+        merged_string += word1[index];
+        merged_string += word2[index];
+    }
+    merged_string += word1.substring(index);
+    merged_string += word2.substring(index);
+    return merged_string;
+};
+
+export const test_funtions = [
+    mergeAlternately,
+    mergeAlternately1,
+    mergeAlternately2,
+];
