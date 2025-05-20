f0b9774eb07659ebb471e4afd0c68d16edf13405
yuanhao <fyuanhao91@gmail.com>
2025-05-20 18:27:22 +0800

    [Algorithms-283] typescript solution.

---
+++ Algorithms/TypeScript/283. Move Zeroes/283. Move Zeroes.spec.ts
@@ -0,0 +1,35 @@
+﻿import { TestCase, test_template } from '../interface/testCase';
+import { test_funtions } from './283. Move Zeroes';
+
+const testCases: TestCase[] = [
+    {
+        name: 'Example 1',
+        inputs: [[0,1,0,3,12]],
+        expect_result: ([1,3,12,0,0]),
+    },
+    {
+        name: 'Example 2',
+        inputs: [[0]],
+        expect_result: ([0]),
+    },
+    {
+        name: 'Example 3',
+        inputs: [[0, 1, 0, 3, 12, 0, 0, 0, 0, 0, 0, 0, -1, -2, 89, 7, 5, 4, 0, 7,0, 67, 0, 7, 0]],
+        expect_result: ([1, 3, 12, -1, -2, 89, 7, 5, 4, 7, 67, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+    },
+    {
+        name: 'Example 4',
+        inputs: [[1]],
+        expect_result: ([1]),
+    },
+    {
+        name: 'Example 5',
+        inputs: [[1, 1]],
+        expect_result: ([1, 1]),
+    },
+];
+
+test_funtions.forEach((fnc) => {
+    const template = test_template(fnc);
+    testCases.forEach((testCase) => template(testCase));
+});
---
+++ Algorithms/TypeScript/283. Move Zeroes/283. Move Zeroes.ts
@@ -0,0 +1,19 @@
+﻿function moveZeroes(nums: number[]): number[] {
+    let zero_index = nums.indexOf(0);
+    for (let i = (zero_index >= 0 ? zero_index : nums.length) + 1; i < nums.length; ++i) {
+        if (nums[i]) {
+            nums[zero_index] = nums[i];
+            nums[i] = 0;
+            zero_index++;
+        }
+    }
+    return nums;
+};
+
+// function moveZeroes(nums: number[]): void {
+    
+// };
+
+export const test_funtions = [
+    moveZeroes,
+];
