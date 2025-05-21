b82b2976e6a3bc9234bef79504e1dbed2bdfa22b
yuanhao <fyuanhao91@gmail.com>
2025-05-21 14:00:41 +0800

    [Algorithms-73] typescript solution.

---
+++ Algorithms/TypeScript/73. Set Matrix Zeroes/73. Set Matrix Zeroes.spec.ts
@@ -0,0 +1,20 @@
+﻿import { TestCase, test_template } from '../interface/testCase';
+import { test_funtions } from './73. Set Matrix Zeroes';
+
+test_funtions.forEach((fnc) => {
+    const testCases: TestCase[] = [
+        {
+            name: 'Example 1',
+            inputs: [[[1,1,1],[1,0,1],[1,1,1]]],
+            expect_result: ([[1,0,1],[0,0,0],[1,0,1]]),
+        },
+        {
+            name: 'Example 2',
+            inputs: [[[0,1,2,0],[3,4,5,2],[1,3,1,5]]],
+            expect_result: ([[0,0,0,0],[0,4,5,0],[0,3,1,0]]),
+        },
+    ];
+
+    const template = test_template(fnc);
+    testCases.forEach((testCase) => template(testCase));
+});
---
+++ Algorithms/TypeScript/73. Set Matrix Zeroes/73. Set Matrix Zeroes.ts
@@ -0,0 +1,58 @@
+﻿// function setZeroes(matrix: number[][]): void {
+    
+// };
+
+function setZeroes(matrix: number[][]): number[][] {
+    let zero_row = new Set(), zero_column = new Set();
+    for (let row = 0; row < matrix.length; ++row) {
+        for (let col = 0; col < matrix[row].length; ++col) {
+            if (matrix[row][col] == 0) {
+                zero_row.add(row);
+                zero_column.add(col);
+            }
+        }
+    }
+    for (let row = 0; row < matrix.length; ++row) {
+        if (zero_row.has(row)) {
+            for (let col = 0; col < matrix[row].length; ++col)
+                matrix[row][col] = 0;
+        }
+    }
+    for (let col = 0; col < matrix[0].length; ++col) {
+        if (zero_column.has(col)) {
+            for (let row = 0; row < matrix.length; ++row)
+                matrix[row][col] = 0;
+        }
+    }
+    
+    return matrix;
+};
+
+function setZeroes1(matrix: number[][]): number[][] {
+    let set_first_col = false;
+    for (let row = 0; row < matrix.length; ++row) {
+        if (matrix[row][0] == 0)
+            set_first_col = true;
+        for (let col = 1; col < matrix[row].length; ++col) {
+            if (matrix[row][col] == 0) {
+                matrix[row][0] = 0;
+                matrix[0][col] = 0;
+            }
+        }
+    }
+    for (let row = matrix.length - 1; row >= 0; --row) {
+        for (let col = matrix[row].length - 1; col >= 1; --col) {
+            if (matrix[row][0] == 0 || matrix[0][col] == 0)
+                matrix[row][col] = 0;
+        }
+        if (set_first_col)
+            matrix[row][0] = 0;
+    }
+
+    return matrix;
+};
+
+export const test_funtions = [
+    setZeroes,
+    setZeroes1,
+];
