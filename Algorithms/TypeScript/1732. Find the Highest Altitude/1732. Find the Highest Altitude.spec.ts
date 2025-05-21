ed9834d39c4bdaa8766f41cd380fd0446b61df30
yuanhao <fyuanhao91@gmail.com>
2025-05-21 17:13:21 +0800

    [Algorithms-1732] typescript solution.

---
+++ Algorithms/TypeScript/1732. Find the Highest Altitude/1732. Find the Highest Altitude.spec.ts
@@ -0,0 +1,20 @@
+﻿import { TestCase, test_template } from '../interface/testCase';
+import { test_funtions } from './1732. Find the Highest Altitude';
+
+test_funtions.forEach((fnc) => {
+    const testCases: TestCase[] = [
+        {
+            name: 'Example 1',
+            inputs: [[-5,1,5,0,-7]],
+            expect_result: 1,
+        },
+        {
+            name: 'Example 2',
+            inputs: [[-4,-3,-2,-1,4,3,2]],
+            expect_result: 0,
+        },
+    ];
+
+    const template = test_template(fnc);
+    testCases.forEach((testCase) => template(testCase));
+});
---
+++ Algorithms/TypeScript/1732. Find the Highest Altitude/1732. Find the Highest Altitude.ts
@@ -0,0 +1,12 @@
+﻿function largestAltitude(gain: number[]): number {
+    let largest = 0, current_altitude = 0;
+    for (let i = 0; i < gain.length; ++i) {
+        current_altitude += gain[i];
+        largest = Math.max(largest, current_altitude);
+    }
+    return largest;
+};
+
+export const test_funtions = [
+    largestAltitude,
+];
