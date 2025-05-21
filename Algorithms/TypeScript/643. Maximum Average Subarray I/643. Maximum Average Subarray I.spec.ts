685a67fd3a5577291fa6184eafa44e480afdbfa2
yuanhao <fyuanhao91@gmail.com>
2025-05-21 17:05:22 +0800

    [Algorithms-643] typescript solution.

---
+++ Algorithms/TypeScript/643. Maximum Average Subarray I/643. Maximum Average Subarray I.spec.ts
@@ -0,0 +1,35 @@
+﻿import { TestCase, test_template } from '../interface/testCase';
+import { test_funtions } from './643. Maximum Average Subarray I';
+
+test_funtions.forEach((fnc) => {
+    const testCases: TestCase[] = [
+        {
+            name: 'Example 1',
+            inputs: [[1,12,-5,-6,50,3], 4],
+            expect_result: 12.75,
+        },
+        {
+            name: 'Example 2',
+            inputs: [[5], 1],
+            expect_result: 5,
+        },
+        {
+            name: 'Example 3',
+            inputs: [[5, -1], 1],
+            expect_result: 5,
+        },
+        {
+            name: 'Example 4',
+            inputs: [[5, -1, 10], 1],
+            expect_result: 10,
+        },
+        {
+            name: 'Example 5',
+            inputs: [[5, -1, 10], 3],
+            expect_result: 4.666666666666667,
+        },
+    ];
+
+    const template = test_template(fnc);
+    testCases.forEach((testCase) => template(testCase));
+});
---
+++ Algorithms/TypeScript/643. Maximum Average Subarray I/643. Maximum Average Subarray I.ts
@@ -0,0 +1,17 @@
+﻿function findMaxAverage(nums: number[], k: number): number {
+    let max_sum = 0, current_sum = 0;
+    for (let i = 0; i < k; ++i)
+        max_sum += nums[i];
+
+    current_sum = max_sum;
+    for (let i = k; i < nums.length; ++i) {
+        current_sum -= nums[i - k];
+        current_sum += nums[i];
+        max_sum = Math.max(max_sum, current_sum);
+    }
+    return max_sum / k;
+};
+
+export const test_funtions = [
+    findMaxAverage,
+];
