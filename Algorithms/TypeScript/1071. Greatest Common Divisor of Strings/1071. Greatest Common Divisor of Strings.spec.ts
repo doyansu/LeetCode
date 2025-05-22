491466920e0c71aab8729c980d544527ea596c26
yuanhao <fyuanhao91@gmail.com>
2025-05-23 01:22:40 +0800

    [Algorithms-1071] typescript solution(string gcd) .

---
+++ Algorithms/TypeScript/1071. Greatest Common Divisor of Strings/1071. Greatest Common Divisor of Strings.spec.ts
@@ -0,0 +1,25 @@
+﻿import { TestCase, test_template } from '../interface/testCase';
+import { test_funtions } from './1071. Greatest Common Divisor of Strings';
+
+test_funtions.forEach((fnc) => {
+    const testCases: TestCase[] = [
+        {
+            name: 'Example 1',
+            inputs: ["ABCABC", "ABC"],
+            expect_result: "ABC",
+        },
+        {
+            name: 'Example 2',
+            inputs: ["ABABAB", "ABAB"],
+            expect_result: "AB",
+        },
+        {
+            name: 'Example 3',
+            inputs: ["LEET", "CODE"],
+            expect_result: "",
+        },
+    ];
+
+    const template = test_template(fnc);
+    testCases.forEach((testCase) => template(testCase));
+});
---
+++ Algorithms/TypeScript/1071. Greatest Common Divisor of Strings/1071. Greatest Common Divisor of Strings.ts
@@ -0,0 +1,39 @@
+﻿function gcd(n1: number, n2: number): number  {
+    return (n1 % n2) ? gcd(n2, n1 % n2) : n2;
+}
+
+function gcdOfStrings(str1: string, str2: string): string {
+    if (str1.length < str2.length)
+        [str1, str2] = [str2, str1];
+    let gcd_length = gcd(str1.length, str2.length);
+    for (let factor = gcd_length; factor > 0; --factor) {
+        if (gcd_length % factor == 0) {
+            let i;
+            for(i = factor; i < str2.length; i += factor){
+                if(str2.substring(0, factor) != str2.substring(i, i + factor))
+                    break;
+            }
+            if(i != str2.length)
+                continue;
+            for(i = 0; i < str1.length; i += factor){
+                if(str2.substring(0, factor) != str1.substring(i, i + factor))
+                    break;
+            }
+            if(i == str1.length){
+                return str2.substring(0, factor);
+            }
+        }
+    }
+    return '';
+};
+
+function gcdOfStrings1(str1: string, str2: string): string {
+    if (str1 + str2 != str2 + str1)
+        return '';
+    return str1.substring(0, gcd(str1.length, str2.length));
+};
+
+export const test_funtions = [
+    gcdOfStrings,
+    gcdOfStrings1,
+];
