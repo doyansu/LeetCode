5f1cfb54bf092ef1f4c38100c48472218f2c3360
yuanhao <fyuanhao91@gmail.com>
2025-05-23 00:17:00 +0800

    [Algorithms-21] typescript solution(merge listnode) .

---
+++ Algorithms/TypeScript/21. Merge Two Sorted Lists/21. Merge Two Sorted Lists.spec.ts
@@ -0,0 +1,39 @@
+﻿import { TestCase, test_template } from '../interface/testCase';
+import { test_funtions, ListNode } from './21. Merge Two Sorted Lists';
+
+function arrayToListNode(arr: number[]): ListNode | null {
+    if (arr.length === 0) return null;
+    
+    const head = new ListNode(arr[0]);
+    let current = head;
+    
+    for (let i = 1; i < arr.length; i++) {
+        current.next = new ListNode(arr[i]);
+        current = current.next;
+    }
+    
+    return head;
+}
+
+test_funtions.forEach((fnc) => {
+    const testCases: TestCase[] = [
+        {
+            name: 'Example 1',
+            inputs: [arrayToListNode([1,2,4]), arrayToListNode([1,3,4])],
+            expect_result: arrayToListNode([1,1,2,3,4,4]),
+        },
+        {
+            name: 'Example 2',
+            inputs: [arrayToListNode([]), arrayToListNode([])],
+            expect_result: arrayToListNode([]),
+        },
+        {
+            name: 'Example 3',
+            inputs: [arrayToListNode([]), arrayToListNode([0])],
+            expect_result: arrayToListNode([0]),
+        },
+    ];
+
+    const template = test_template(fnc);
+    testCases.forEach((testCase) => template(testCase));
+});
---
+++ Algorithms/TypeScript/21. Merge Two Sorted Lists/21. Merge Two Sorted Lists.ts
@@ -0,0 +1,51 @@
+﻿// Definition for singly-linked list.
+export class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+
+function merge(nums1: number[], nums2: number[]): number[] {
+    let new_array: number[] = [];
+    let index1 = 0, index2 = 0;
+    while (index1 < nums1.length && index2 < nums2.length) 
+        new_array.push(nums1[index1] <= nums2[index2] ? nums1[index1++] : nums2[index2++]);
+    while (index1 < nums1.length)
+        new_array.push(nums1[index1++]);
+    while (index2 < nums2.length)
+        new_array.push(nums2[index2++]);
+    return new_array;
+}
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    let root: ListNode = new ListNode();
+    let current: ListNode = root;
+    while (list1 && list2) {
+        if (list1.val <= list2.val) {
+            current.next = list1;
+            list1 = list1.next;
+        } else {
+            current.next = list2;
+            list2 = list2.next;
+        }
+        current = current.next
+    }
+    while (list1) {
+        current.next = list1;
+        list1 = list1.next;
+        current = current.next
+    }
+    while (list2) {
+        current.next = list2;
+        list2 = list2.next;
+        current = current.next
+    }
+    return root.next;
+};
+
+export const test_funtions = [
+    mergeTwoLists,
+];
