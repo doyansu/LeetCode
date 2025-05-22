eb2df9ac4a3ba90a7d3453dceb5d5d1350853831
yuanhao <fyuanhao91@gmail.com>
2025-05-23 01:47:31 +0800

    [Algorithms-933] typescript solution.

---
+++ Algorithms/TypeScript/933. Number of Recent Calls/933. Number of Recent Calls.spec.ts
@@ -0,0 +1,15 @@
+﻿import { TestCase, test_template } from '../interface/testCase';
+import { test_funtions } from './933. Number of Recent Calls';
+
+test_funtions.forEach((fnc) => {
+    const testCases: TestCase[] = [
+        {
+            name: 'Example 1',
+            inputs: [[[1], [100], [3001], [3002]]],
+            expect_result: [1, 2, 3, 3],
+        },
+    ];
+
+    const template = test_template(fnc);
+    testCases.forEach((testCase) => template(testCase));
+});
