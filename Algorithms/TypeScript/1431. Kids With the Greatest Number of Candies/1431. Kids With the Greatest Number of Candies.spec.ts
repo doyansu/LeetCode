5df919a89d9f009e858016ddeb73c022ae6bd15a
yuanhao <fyuanhao91@gmail.com>
2025-05-24 00:31:25 +0800

    [Algorithms-1431] typescript solution.

---
+++ Algorithms/TypeScript/1431. Kids With the Greatest Number of Candies/1431. Kids With the Greatest Number of Candies.spec.ts
@@ -0,0 +1,25 @@
+﻿import { TestCase, test_template } from '../interface/testCase';
+import { test_funtions } from './1431. Kids With the Greatest Number of Candies';
+
+test_funtions.forEach((fnc) => {
+    const testCases: TestCase[] = [
+        {
+            name: 'Example 1',
+            inputs: [[2,3,5,1,3], 3],
+            expect_result: [true,true,true,false,true],
+        },
+        {
+            name: 'Example 2',
+            inputs: [[4,2,1,1,2], 1],
+            expect_result: [true,false,false,false,false],
+        },
+        {
+            name: 'Example 3',
+            inputs: [[12,1,12], 10],
+            expect_result: [true,false,true],
+        },
+    ];
+
+    const template = test_template(fnc);
+    testCases.forEach((testCase) => template(testCase));
+});
---
+++ Algorithms/TypeScript/1431. Kids With the Greatest Number of Candies/1431. Kids With the Greatest Number of Candies.ts
@@ -0,0 +1,15 @@
+﻿function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
+    let result: boolean[] = [];
+    let max_candies = 0; // Math.max(...candies);
+    for (let kid = 0; kid < candies.length; ++kid) {
+        max_candies = Math.max(max_candies, candies[kid]);
+    }
+    for (let kid = 0; kid < candies.length; ++kid) {
+        result.push(candies[kid] + extraCandies >= max_candies);
+    }
+    return result;
+};
+
+export const test_funtions = [
+    kidsWithCandies,
+];
