703a49b92e00feaac14f29e95cd28b5fdda11c65
yuanhao <fyuanhao91@gmail.com>
2025-05-24 00:58:18 +0800

    [Algorithms-392] typescript solution.

---
+++ Algorithms/TypeScript/392. Is Subsequence/392. Is Subsequence.spec.ts
@@ -0,0 +1,30 @@
+﻿import { TestCase, test_template } from '../interface/testCase';
+import { test_funtions } from './392. Is Subsequence';
+
+test_funtions.forEach((fnc) => {
+    const testCases: TestCase[] = [
+        {
+            name: 'Example 1',
+            inputs: ["abc", "ahbgdc"],
+            expect_result: true,
+        },
+        {
+            name: 'Example 2',
+            inputs: ["axc", "ahbgdc"],
+            expect_result: false,
+        },
+        {
+            name: 'Example 3',
+            inputs: ["", ""],
+            expect_result: true,
+        },
+        {
+            name: 'Example 4',
+            inputs: ["aaaaaa", "bbaaaa"],
+            expect_result: false,
+        },
+    ];
+
+    const template = test_template(fnc);
+    testCases.forEach((testCase) => template(testCase));
+});
---
+++ Algorithms/TypeScript/392. Is Subsequence/392. Is Subsequence.ts
@@ -0,0 +1,28 @@
+﻿function isSubsequence(s: string, t: string): boolean {
+    let index = 0;
+    for (const char of s) {
+        while (char != t[index]) {
+            if (++index >= t.length)
+                return false;
+        }
+        ++index;
+    }
+
+    return true;
+};
+
+function isSubsequence1(s: string, t: string): boolean {
+    let i = 0;
+    for (const char of t) {
+        if (char === s[i]) 
+            ++i;
+        if (i === s.length) 
+            return true;
+    }
+    return i === s.length;
+};
+
+export const test_funtions = [
+    isSubsequence,
+    isSubsequence1,
+];
