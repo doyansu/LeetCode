3e192da0b82307cb16107999ac839c4c3e79563f
yuanhao <fyuanhao91@gmail.com>
2025-05-26 20:47:03 +0800

    [Algorithms-2215] typescript solution(set).

---
+++ Algorithms/TypeScript/2215. Find the Difference of Two Arrays/2215. Find the Difference of Two Arrays.spec.ts
@@ -0,0 +1,20 @@
+﻿import { TestCase, test_template } from '../interface/testCase';
+import { test_funtions } from './2215. Find the Difference of Two Arrays';
+
+test_funtions.forEach((fnc) => {
+    const testCases: TestCase[] = [
+        {
+            name: 'Example 1',
+            inputs: [[1,2,3], [2,4,6]],
+            expect_result: [[1,3],[4,6]],
+        },
+        {
+            name: 'Example 2',
+            inputs: [[1,2,3,3], [1,1,2,2]],
+            expect_result: [[3],[]],
+        },
+    ];
+
+    const template = test_template(fnc);
+    testCases.forEach((testCase) => template(testCase));
+});
---
+++ Algorithms/TypeScript/2215. Find the Difference of Two Arrays/2215. Find the Difference of Two Arrays.ts
@@ -0,0 +1,24 @@
+﻿function findDifference(nums1: number[], nums2: number[]): number[][] {
+    let set1 = new Set(nums1), set2 = new Set(nums2);
+
+    let f = (s1: Set<number>, s2: Set<number>) => {
+        s1.forEach((value) => {
+            if (s2.has(value))
+            {
+                s1.delete(value);
+                s2.delete(value);
+            }
+        });
+    };
+
+    if (set1.size < set2.size)
+        f(set1, set2);
+    else
+        f(set2, set1);
+
+    return [[...set1], [...set2]];
+};
+
+export const test_funtions = [
+    findDifference,
+];
