af048adc3ad5779909e42ca97caed4da3cb9398c
yuanhao <fyuanhao91@gmail.com>
2025-05-27 18:08:52 +0800

    [Algorithms-338] typescript solution.

---
+++ Algorithms/TypeScript/338. Counting Bits/338. Counting Bits.spec.ts
@@ -0,0 +1,20 @@
+﻿import { TestCase, test_template } from '../interface/testCase';
+import { test_funtions } from './338. Counting Bits';
+
+test_funtions.forEach((fnc) => {
+    const testCases: TestCase[] = [
+        {
+            name: 'Example 1',
+            inputs: [2],
+            expect_result: [0,1,1],
+        },
+        {
+            name: 'Example 2',
+            inputs: [5],
+            expect_result: [0,1,1,2,1,2],
+        },
+    ];
+
+    const template = test_template(fnc);
+    testCases.forEach((testCase) => template(testCase));
+});
---
+++ Algorithms/TypeScript/338. Counting Bits/338. Counting Bits.ts
@@ -0,0 +1,56 @@
+﻿function lowbit(n: number){
+    return n & -n;
+}
+
+// 3ms
+function countBits(n: number): number[] {
+    let result: number[] = new Array(n + 1).fill(0);
+    for (let i = 1; i <= n; ++i) {
+        let temp = i;
+        while(temp){
+            temp ^= lowbit(temp);
+            ++result[i];
+        }
+    }
+
+    return result;
+};
+
+// 1ms
+function countBits1(n: number): number[] {
+    let result: number[] = new Array(n + 1);
+    result[0] = 0;
+    for (let i = 1; i <= n; ++i) {
+        result[i] = result[i ^ lowbit(i)] + 1;
+    }
+
+    return result;
+};
+
+let bit_list: number[] = new Array(100001).fill(0);
+for (let i = 1; i <= 100001; ++i) {
+    bit_list[i] = bit_list[i ^ lowbit(i)] + 1;
+}
+
+// 1ms
+function countBits2(n: number): number[] {
+    return bit_list.slice(0, n + 1);
+};
+
+// 1ms
+function countBits3(n: number): number[] {
+    let result: number[] = new Array(n + 1);
+    result[0] = 0;
+    for (let i = 1; i <= n; ++i) {
+        result[i] = result[i>>1] + (i&1);
+    }
+
+    return result;
+};
+
+export const test_funtions = [
+    countBits,
+    countBits1,
+    countBits2,
+    countBits3,
+];
