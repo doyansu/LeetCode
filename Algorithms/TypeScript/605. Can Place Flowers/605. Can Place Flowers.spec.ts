c3a8dd5fac50f3c75a888ab43c3fba5fa67cba99
yuanhao <fyuanhao91@gmail.com>
2025-05-29 21:27:12 +0800

    [Algorithm-605] typescript solution.

---
+++ Algorithms/TypeScript/605. Can Place Flowers/605. Can Place Flowers.spec.ts
@@ -0,0 +1,46 @@
+﻿import { TestCase, test_template, test_template_verify_function } from '../interface/testCase';
+import { test_funtion_list, verify_function } from './605. Can Place Flowers';
+
+test_funtion_list.forEach((fnc) => {
+    const test_case_list: TestCase[] = [
+        {
+            name: 'Example 1',
+            inputs: [[1,0,0,0,1], 1],
+            expect_result: true,
+        },
+        {
+            name: 'Example 2',
+            inputs: [[1,0,0,0,1], 2],
+            expect_result: false,
+        },
+        {
+            name: 'Example 3',
+            inputs: [[1,0,0,0,0], 2],
+            expect_result: true,
+        },
+        {
+            name: 'Example 4',
+            inputs: [[0,0,0,0,0], 4],
+            expect_result: false,
+        },
+        {
+            name: 'Example 5',
+            inputs: [[1,0,1,0,1,0,1], 0],
+            expect_result: true,
+        },
+        {
+            name: 'Example 6',
+            inputs: [[0,0,0,0,0,1,0,0], 0],
+            expect_result: true,
+        },
+    ];
+
+    const template = test_template(fnc);
+    const template_verify = test_template_verify_function(fnc, verify_function);
+    
+    test_case_list.forEach((testCase) => template(testCase));
+
+    if (verify_function !== undefined) {
+        test_case_list.forEach((testCase) => template_verify(testCase));
+    }
+});
---
+++ Algorithms/TypeScript/605. Can Place Flowers/605. Can Place Flowers.ts
@@ -0,0 +1,22 @@
+﻿function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+    const can_place_flower = (index: number): boolean => {
+        return (index == 0 || flowerbed[index - 1] == 0) && flowerbed[index] == 0 && (index == flowerbed.length - 1 || flowerbed[index + 1] == 0);
+    };
+
+    for (let i = 0; i < flowerbed.length; ++i) {
+        if (can_place_flower(i)) {
+            flowerbed[i] = 1;
+            --n;
+        }
+        if (n <= 0)
+            return true;
+    }
+
+    return false;
+};
+
+export const test_funtion_list = [
+    canPlaceFlowers,
+];
+
+export const verify_function = undefined;
