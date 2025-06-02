920ba62c854a45ce66654958fdcbe0c6c17c5f24
yuanhao <fyuanhao91@gmail.com>
2025-06-02 14:06:45 +0800

    [Algorithms-374] typescript solution(binary search).

---
+++ Algorithms/TypeScript/374. Guess Number Higher or Lower/374. Guess Number Higher or Lower.spec.ts
@@ -0,0 +1,56 @@
+﻿import { TestCase, test_template } from '../interface/testCase';
+import { getRandomInt } from '../interface/commonFuntions';
+import { test_funtion_list, verify_function } from './374. Guess Number Higher or Lower';
+
+test_funtion_list.forEach((fnc) => {
+    // default test case
+    const test_case_list: TestCase[] = [
+        {
+            name: 'Example 1',
+            inputs: [10, 6],
+            expect_result: 6,
+        },
+        {
+            name: 'Example 2',
+            inputs: [1, 1],
+            expect_result: 1,
+        },
+        {
+            name: 'Example 3',
+            inputs: [2, 1],
+            expect_result: 1,
+        },
+        {
+            name: 'Example 4',
+            inputs: [4143848, 1],
+            expect_result: 1,
+        },
+        {
+            name: 'Example 5',
+            inputs: [412943908, 412943908],
+            expect_result: 412943908,
+        },
+        {
+            name: 'Example 14',
+            inputs: [2126753390, 1702766719],
+            expect_result: 1702766719,
+        },
+    ];
+
+    // init romdom test case
+    if (verify_function !== undefined) {
+        for (let i = 0; i < 0; ++i) {
+            let inputs: any[] = [getRandomInt(0, 1)];
+            let random_test_case: TestCase = {
+                name: `Random Example ${i}`,
+                inputs: inputs,
+                expect_result: verify_function(...inputs),
+            };
+            test_case_list.push(random_test_case);
+        }
+    }
+
+    // start test
+    const template = test_template(fnc);
+    test_case_list.forEach((testCase) => template(testCase));
+});
---
+++ Algorithms/TypeScript/374. Guess Number Higher or Lower/374. Guess Number Higher or Lower.ts
@@ -0,0 +1,36 @@
+﻿//  * Forward declaration of guess API.
+//  * @param {number} num   your guess
+//  * @return 	     -1 if num is higher than the picked number
+//  *			      1 if num is lower than the picked number
+//  *               otherwise return 0
+//  * var guess = function(num) {}
+
+let guess = (guess_number: number): number => 1 - guess_number;
+
+function guessGame(n: number, pick: number) {
+    guess = (guess_number: number): number => pick - guess_number;
+    return guessNumber(n);
+}
+
+function guessNumber(n: number): number {
+    let left = 1;
+    let right = n;
+    while (left < right) {
+        let guess_number = (left + right) >>> 1;
+        let guess_result = guess(guess_number);
+        if (guess_result == 0)
+            return guess_number;
+        else if (guess_result > 0)
+            left = guess_number + 1;
+        else
+            right = guess_number - 1;
+    }
+
+    return left;
+};
+
+export const test_funtion_list = [
+    guessGame,
+];
+
+export const verify_function: any = undefined;
