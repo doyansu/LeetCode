5bbcd59dfcf980733d60a3ba5ac5b04082089e60
yuanhao <fyuanhao91@gmail.com>
2025-06-07 12:40:30 +0800

    [TypeScript] 新增 get_random_string 函式生成隨機字串。

--- Algorithms/TypeScript/interface/commonFuntions.ts
+++ Algorithms/TypeScript/interface/commonFuntions.ts
@@ -5,6 +5,34 @@ export function get_random_integer(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// 可快速指定隨機字串集合
+export enum RandomStringType {
+    Numeric = '0123456789',
+    LowerAlpha = 'abcdefghijklmnopqrstuvwxyz',
+    UpperAlpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    Alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+    AlphaNumeric = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
+}
+
+// 取得隨機字串 (長度 = length)
+export function get_random_string(length: number, stringSet: RandomStringType | string = RandomStringType.LowerAlpha, isFastMode: boolean = true): string {
+    return isFastMode ? get_random_string_fastmode(length, stringSet) : get_random_string_normalmode(length, stringSet);
+}
+
+function get_random_string_fastmode(length: number, stringSet: string) {
+    let result = '';
+    for (let i = 0; i < length; ++i) 
+        result += stringSet[get_random_integer(0, stringSet.length - 1)];
+    return result;
+}
+
+function get_random_string_normalmode(length: number, stringSet: string) {
+    return new Array(length)
+        .fill('')
+        .map(() => stringSet[get_random_integer(0, stringSet.length - 1)])
+        .join('');;
+}
+
 // 將 array 轉換成 ListNode
 export function transform_array_to_listNode(arr: number[]): ListNode | null {
     if (arr.length === 0) return null;
@@ -20,7 +48,7 @@ export function transform_array_to_listNode(arr: number[]): ListNode | null {
     return head;
 }
 
-// 
+// 將二元堆轉換成二元樹節點
 export function transform_binaryheap_to_binaryTree(arr: (number | null)[]): TreeNode | null {
     if (arr.length === 0) 
         return null;
