98386ad64e9d6c4b1efb3e766f0a563c71985ec7
yuanhao <fyuanhao91@gmail.com>
2025-06-24 00:23:07 +0800

    [Algorithms-1004] typescript one space solution(Sliding Window).

--- Algorithms/TypeScript/1004. Max Consecutive Ones III/1004. Max Consecutive Ones III.ts
+++ Algorithms/TypeScript/1004. Max Consecutive Ones III/1004. Max Consecutive Ones III.ts
@@ -1,6 +1,25 @@
 import { Queue } from '../interface/myDataStructure/myQueue'
 
 function longestOnes(nums: number[], k: number): number {
+    let left = 0;
+
+    for (let right = 0; right < nums.length; ++right) {
+        if (nums[right] == 0) {
+            --k;
+        }
+        
+        if (k < 0) {
+            if (nums[left] == 0)
+                ++k;
+
+            ++left;
+        }
+    }
+
+    return nums.length - left;
+};
+
+function longestOnes_queue(nums: number[], k: number): number {
     let longest_one = 0;
     let last_zero_index = -1;
     let zero_queue: Queue<number> = new Queue();
@@ -21,6 +40,7 @@ function longestOnes(nums: number[], k: number): number {
 
 export const test_funtion_list = [
     longestOnes,
+    longestOnes_queue,
 ];
 
 export const verify_function: any = undefined;
