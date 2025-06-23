9d423328798ab1bd3cc9a5c82f42f06949a32218
yuanhao <fyuanhao91@gmail.com>
2025-06-24 00:17:33 +0800

    [Algorithms-1004] typescript solution(Sliding Window) 使用 linked list queue 加速.

--- Algorithms/TypeScript/1004. Max Consecutive Ones III/1004. Max Consecutive Ones III.ts
+++ Algorithms/TypeScript/1004. Max Consecutive Ones III/1004. Max Consecutive Ones III.ts
@@ -1,13 +1,15 @@
+import { Queue } from '../interface/myDataStructure/myQueue'
+
 function longestOnes(nums: number[], k: number): number {
     let longest_one = 0;
     let last_zero_index = -1;
-    let zero_queue = [];
+    let zero_queue: Queue<number> = new Queue();
 
     for (let i = 0; i < nums.length; ++i) {
         if (nums[i] == 0) {
-            zero_queue.push(i);
-            if (zero_queue.length > k) {
-                last_zero_index = zero_queue.shift()!;
+            zero_queue.enqueue(i);
+            if (zero_queue.size() > k) {
+                last_zero_index = zero_queue.dequeue()!;
             }
         }
         
